309cd262c66cd0a75c0da4a5ce9deb043a866a99
Samuel Drell <iwn18@txstate.edu>
2026-04-17 22:25:57 -0500

    Changes input file name expectations

--- main.cpp
+++ main.cpp
@@ -7,44 +7,45 @@ using namespace std;
 const int MAX_SIZE = 1000;
 
 void fileio(string filename) {
-  /* create input and output filenames */
-  string input_filename = filename + "_input";
-  string output_filename = filename + "_output";
+    /* create input and output filenames */
+    //   string input_filename = filename + "_input";
+    string input_filename = filename;
+    string output_filename = filename + "_output";
 
-  /* create input and output files streams */
-  ofstream output;
-  ifstream input;
+    /* create input and output files streams */
+    ofstream output;
+    ifstream input;
 
 
-  /* open input stream for reading */
-  input.open(input_filename);
+    /* open input stream for reading */
+    input.open(input_filename);
 
-  /* check if input stream has been succesfully opened; bail otherwise */
-  if (!input.is_open()) {
-    cerr << "Could not open input file " << input_filename << ". Exiting ..." << endl;
-    exit(0);
-  }
+    /* check if input stream has been succesfully opened; bail otherwise */
+    if (!input.is_open()) {
+        cerr << "Could not open input file " << input_filename << ". Exiting ..." << endl;
+        exit(0);
+    }
+        
+    unsigned long *nums = new unsigned long[MAX_SIZE];
+    int count = 0;
+    while (!input.eof() && count < MAX_SIZE) {
+        input >> nums[count];
+        count++;
+    }
+
+    /* done reading from input file; close the stream */
+    input.close();
     
-  unsigned long *nums = new unsigned long[MAX_SIZE];
-  int count = 0;
-  while (!input.eof() && count < MAX_SIZE) {
-    input >> nums[count];
-    count++;
-  }
+    /* open output file stream for writing */
+    output.open(output_filename);
 
-  /* done reading from input file; close the stream */
-  input.close();
-  
-  /* open output file stream for writing */
-  output.open(output_filename);
+    /* no need to check if output file is open; we will create new file if it doesn't exist; 
+        overwrite otherwise */
+    for (int i = 0; i < count; i++)
+        output << "ADDR : " << nums[i] << endl; 
 
-  /* no need to check if output file is open; we will create new file if it doesn't exist; 
-     overwrite otherwise */
-  for (int i = 0; i < count; i++)
-    output << "ADDR : " << nums[i] << endl; 
-
-  /* close output stream */
-  output.close();
+    /* close output stream */
+    output.close();
 }
 
 int main(int argc, char*argv[]) {

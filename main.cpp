bde55f8c53de2ca2416b9d47262367eed4dcaf2f
Samuel Drell <iwn18@txstate.edu>
2026-04-17 22:08:32 -0500

    Adds filename parameter to fileio helper

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-ifstream fileio() {
+ifstream fileio(string filename) {
   /* create input and output filenames */
   string input_filename = filename + "_input";
   string output_filename = filename + "_output";
@@ -49,25 +49,26 @@ ifstream fileio() {
 
 int main(int argc, char*argv[]) {
 
-  /* check if the correct number of arguments have been passed; bail otherwise */
-  if (argc < 4) {
-    cout << "Usage: " << endl;
-    cout << "   ./cache num_entries associativity filename" << endl;
+    /* check if the correct number of arguments have been passed; bail otherwise */
+    if (argc < 4) {
+        cout << "Usage: " << endl;
+        cout << "   ./cache num_entries associativity filename" << endl;
+        return 0;
+    }
+
+    /* get args; all args come in as strings; need to convert to int with atoi() */
+    unsigned entries = atoi(argv[1]);
+    unsigned assoc = atoi(argv[2]);
+
+    string input_filename = argv[3];
+
+    /* print the args */
+    cout << "Number of entries: " << entries << endl;
+    cout << "Associativity: " << assoc << endl;
+    cout << "Input File Name: " << input_filename << endl;
+    
+    
     return 0;
-  }
-
-  /* get args; all args come in as strings; need to convert to int with atoi() */
-  unsigned entries = atoi(argv[1]);
-  unsigned assoc = atoi(argv[2]);
-
-  string input_filename = argv[3];
-
-  /* print the args */
-  cout << "Number of entries: " << entries << endl;
-  cout << "Associativity: " << assoc << endl;
-  cout << "Input File Name: " << input_filename << endl;
-  
-  return 0;
 }
 
 

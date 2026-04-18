245310958553efb34190d1cf0adcbddcb568892c
Samuel Drell <iwn18@txstate.edu>
2026-04-17 22:46:14 -0500

    Moves existing cache funcitons into private and adds a public check_address function

--- cache_class.cpp
+++ cache_class.cpp
@@ -4,49 +4,53 @@
 using namespace std;
 
 class Entry {
-public:
-  Entry();
-  ~Entry();
-  void display(ofstream& outfile);
+  public:
+    Entry();
+    ~Entry();
+    void display(ofstream& outfile);
 
-  void set_tag(int _tag) { tag = _tag; }
-  int get_tag() { return tag; }
+    void set_tag(int _tag) { tag = _tag; }
+    int get_tag() { return tag; }
 
-  void set_valid(bool _valid) { valid = _valid; }
-  bool get_valid() { return valid; }
+    void set_valid(bool _valid) { valid = _valid; }
+    bool get_valid() { return valid; }
 
-  void set_ref(int _ref) { ref = _ref; }
-  int get_ref() { return ref; }
+    void set_ref(int _ref) { ref = _ref; }
+    int get_ref() { return ref; }
 
-private:  
-  bool valid;
-  unsigned tag;
-  int ref;
+  private:  
+    bool valid;
+    unsigned tag;
+    int ref;
 };
 
 class Cache {
-public:
-  Cache(int, int);
-  ~Cache();
+  public:
+    Cache(int, int);
+    ~Cache();
 
-  void display(ofstream& outfile);
+    bool check_address(unsigned long addr);
+    
+  private:
+    int assoc;
+    unsigned num_entries;
+    int num_sets;
+    vector<vector<Entry>> entries;
 
+    // Do we need this?
+    void display(ofstream& outfile);
 
-  int get_index(unsigned long addr);
-  int get_tag(unsigned long addr);
+    // Do we need this?
+    int get_index(unsigned long addr);
 
-  unsigned long retrieve_addr(int way, int index);
-  
-  bool hit(ofstream& outfile, unsigned long addr);
+    int get_tag(unsigned long addr);
 
-  void update(ofstream& outfile, unsigned long addt);
+    unsigned long retrieve_addr(int way, int index);
+    
+    bool hit(ofstream& outfile, unsigned long addr);
 
-  
-private:
-  int assoc;
-  unsigned num_entries;
-  int num_sets;
-  vector<vector<Entry>> entries;
+    void update(ofstream& outfile, unsigned long addt);
+
 };
 
 
